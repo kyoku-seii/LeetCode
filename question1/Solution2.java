e4f26cac4666c172c017bcbd29922c9aca9229fc
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-04-15 16:12:00 +0900

    增加了leetcode第一问两数之和的解答

--- question1/Solution2.java
+++ question1/Solution2.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Solution2 {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap();
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],i);
         }
