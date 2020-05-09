6cc7a04f49af51c831ed64de5c1e77a4ef8887db
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-05-09 17:13:55 +0900

    345. 反转字符串中的元音字母

--- question344/Solution.java
+++ question344/Solution.java
@@ -1,4 +1,20 @@
 package com.leetcode.question344;
 
 public class Solution {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length-1;
+        while(i<=j){
+            swap(s,i,j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(char[] s, int i, int j) {
+        if(i==j) return;
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
 }
