d1fa056587026bb771e7dc6b16b8d380d3670f0c
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-05-23 22:49:29 +0900

    451. 根据字符出现频率排序

--- question451/Solution.java
+++ question451/Solution.java
@@ -1,4 +1,41 @@
 package com.leetcode.question451;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 public class Solution {
+    public String frequencySort(String s) {
+        if(s==null||s.length()==0)return null;
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            if(!hashMap.containsKey(s.charAt(i))) hashMap.put(s.charAt(i), 1);
+            else hashMap.put(s.charAt(i),hashMap.get(s.charAt(i))+1);
+        }
+
+        PriorityQueue<Character> pq = new PriorityQueue<>(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return hashMap.get(o2)-hashMap.get(o1);
+            }
+        });
+
+        pq.addAll(hashMap.keySet());
+
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()){
+            char temp = pq.poll();
+            int repeat = hashMap.get(temp);
+            for(int j=0;j<repeat;j++){
+                sb.append(temp);
+            }
+        }
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.frequencySort("aaaadwadwa"));
+    }
 }
