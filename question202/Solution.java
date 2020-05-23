9a8af04c109a9a68308668ebcc24e88a375d4a2f
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-05-23 17:53:26 +0900

    202. 快乐数

--- question202/Solution.java
+++ question202/Solution.java
@@ -1,4 +1,22 @@
 package com.leetcode.question202;
 
+import java.util.HashSet;
+
 public class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> result = new HashSet<>();
+        while(!result.contains(n)){
+            if(n==1) return true;
+            result.add(n);
+
+            String strN = String.valueOf(n);
+
+            int sum = 0;
+            for (int i=0;i<strN.length();i++){
+                sum += Integer.parseInt(String.valueOf(strN.charAt(i)))*Integer.parseInt(String.valueOf(strN.charAt(i)));
+            }
+            n = sum;
+        }
+        return false;
+    }
 }
