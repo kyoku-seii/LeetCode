3d004e4e98395f6c1f5efc7ee389d170fee11b96
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-05-24 01:45:04 +0900

    222. 完全二叉树的节点个数

--- question222/Solution222.java
+++ question222/Solution222.java
@@ -1,4 +1,21 @@
 package com.leetcode.question222;
 
 public class Solution222 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int countNodes(TreeNode root) {
+        if(root==null) return 0;
+
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left+right+1;
+    }
 }
