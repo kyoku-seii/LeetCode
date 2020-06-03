de55abf362647de966a95907a9c9970e3095c21c
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-03 21:51:10 +0900

    1431. 拥有最多糖果的孩子

--- question1431/Solution.java
+++ question1431/Solution.java
@@ -1,4 +1,19 @@
 package com.leetcode.question1431;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = -1;
+        for(int i:candies){
+            maxCandies = Math.max(maxCandies,i);
+        }
+
+        LinkedList<Boolean> ans = new LinkedList<>();
+        for (int candy : candies) {
+            ans.add(candy + extraCandies >= maxCandies);
+        }
+        return ans;
+    }
 }
