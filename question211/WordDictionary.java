bbde98a18fe838af8b62dc9655f9d1fbca60a8e6
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-05 21:50:11 +0900

    211. 添加与搜索单词 - 数据结构设计

--- question211/WordDictionary.java
+++ question211/WordDictionary.java
@@ -33,4 +33,25 @@ public class WordDictionary {
         }
         cur.isWord = true;
     }
+
+    public boolean search(String word){
+        return match(root,word,0);
+    }
+
+    private boolean match(Node node, String word, int index){
+        if(index==word.length()) return node.isWord;
+
+        char c = word.charAt(index);
+        if (c != '.') {
+            if(node.next.get(c)==null) return false;
+            return match(node.next.get(c),word,index+1);
+        }else {
+            for(char nextChar:node.next.keySet()){
+                if(match(node.next.get(nextChar),word,index+1))
+                    return true;
+            }
+            return false;
+        }
+
+    }
 }
