5a63833e0ef23d2cbcc56280db9c42b9e491dbe5
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-05 21:35:35 +0900

    211. 添加与搜索单词 - 数据结构设计

--- question211/WordDictionary.java
+++ question211/WordDictionary.java
@@ -1,4 +1,36 @@
 package com.leetcode.question211;
 
+import java.util.TreeMap;
+
 public class WordDictionary {
+    private class Node{
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+        public Node(){
+            this(false);
+        }
+    }
+    private Node root;
+
+    public WordDictionary(){
+        root = new Node();
+
+    }
+
+    public void addWord(String word){
+
+        Node cur = root;
+        for(int i=0;i<word.length();i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c)==null)
+                cur.next.put(c,new Node());
+            cur = cur.next.get(c);
+        }
+        cur.isWord = true;
+    }
 }
