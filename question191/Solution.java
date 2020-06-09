c258920617ab302212824a19643493a0fd35ec71
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-09 22:11:15 +0900

    191. 位1的个数

--- question191/Solution.java
+++ question191/Solution.java
@@ -1,4 +1,14 @@
 package com.leetcode.question191;
 
 public class Solution {
+    public int hammingWeight(int n) {
+        n = Math.abs(n);
+        int count = 0;
+        while(n>0){
+            int end = n&1;
+            if(end==1) count++;
+            n = n >> 1;
+        }
+        return count;
+    }
 }
