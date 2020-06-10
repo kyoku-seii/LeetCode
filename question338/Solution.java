3c318c9cc5a39322cba6579410bffaa7c2f82d72
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-10 16:32:22 +0900

    338. 比特位计数

--- question338/Solution.java
+++ question338/Solution.java
@@ -1,5 +1,22 @@
 package com.leetcode.question338;
 
 public class Solution {
+    public int[] countBits(int num) {
+        int[] res = new int[num+1];
+        for (int i=0;i<=num;i++){
+            int temp = findOne(i);
+            res[i] = temp;
+        }
+        return res;
 
+    }
+
+    private int findOne(int i) {
+        int tempSum = 0;
+        while (i!=0){
+            i = i&(i-1);
+            tempSum++;
+        }
+        return tempSum;
+    }
 }
