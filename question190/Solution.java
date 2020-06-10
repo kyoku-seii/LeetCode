b53d05e46ded2cdd3ccd92fd6f6b6ca989f163e9
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-10 15:47:11 +0900

    190. 颠倒二进制位

--- question190/Solution.java
+++ question190/Solution.java
@@ -1,4 +1,19 @@
 package com.leetcode.question190;
 
+import java.util.Stack;
+
 public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        String num = Integer.toBinaryString(n);
+        Stack<Character> stack = new Stack<>();
+        for (int i=0;i<num.length();i++){
+            stack.add(num.charAt(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return Integer.parseInt(sb.toString());
+    }
 }
