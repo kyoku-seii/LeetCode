12aec60aa0449d5017c408e7c13b9f2404ea44e6
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-15 16:44:51 +0900

    709. 转换成小写字母

--- question709/Solution.java
+++ question709/Solution.java
@@ -1,4 +1,15 @@
 package com.leetcode.question709;
 
-public class Solution {
+class Solution {
+    public String toLowerCase(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(c>=65 && c<=90){
+                c =(char) ((int)c + 32);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }
