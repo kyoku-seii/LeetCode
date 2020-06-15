286e4f51fa3ae75e9e8b037f0993797b4c37682e
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-15 16:56:18 +0900

    58. 最后一个单词的长度

--- question58/Solution.java
+++ question58/Solution.java
@@ -1,4 +1,13 @@
 package com.leetcode.question58;
 
 public class Solution {
+    public int lengthOfLastWord(String s) {
+        int i=s.length()-1;
+        for(;i>=0;i--){
+            if (s.charAt(i)!=' ')break;
+        }
+        String temp = s.substring(0,i+1);
+        String[] res = temp.split(" ");
+        return res[res.length-1].length();
+    }
 }
