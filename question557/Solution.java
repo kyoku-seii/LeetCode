f8de35e643f9a40daef5aa5670f4a355344ee2f7
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-17 13:59:14 +0900

    557. 反转字符串中的单词 III

--- question557/Solution.java
+++ question557/Solution.java
@@ -1,4 +1,13 @@
 package com.leetcode.question557;
 
-public class Solution {
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<words.length;i++){
+            sb.append(new StringBuilder(words[i]).reverse().toString());
+        }
+        return sb.toString();
+    }
 }
